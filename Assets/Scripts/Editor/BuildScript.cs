e8a4228fb9ccdce955cffe6e9bcc0eaddc2c54ac
Chittapon Thongchim <papcoe@gmail.com>
2020-06-13 16:24:30 +0700

    Add SupportFiles

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildScript
+{
+    
+    public static string APP_FOLDER = Directory.GetCurrentDirectory ();
+
+    public static string IOS_FOLDER = string.Format ("{0}/Builds/iOS/", APP_FOLDER);
+
+    static string[] GetScenes () {
+            List<string> scenes = new List<string> ();
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++) {
+                if (EditorBuildSettings.scenes[i].enabled) {
+                    scenes.Add (EditorBuildSettings.scenes[i].path);
+                }
+            }
+            return scenes.ToArray ();
+    }
+
+    static void iOSDevelopment () {
+            PlayerSettings.SetScriptingBackend (BuildTargetGroup.iOS, ScriptingImplementation.IL2CPP);
+            PlayerSettings.applicationIdentifier = "com.pap.unityci";
+            PlayerSettings.SetScriptingDefineSymbolsForGroup (BuildTargetGroup.iOS, "DEV");
+            EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTargetGroup.iOS, BuildTarget.iOS);
+            EditorUserBuildSettings.development = true;
+            BuildReport report = BuildPipeline.BuildPlayer (GetScenes(), IOS_FOLDER, BuildTarget.iOS, BuildOptions.None);
+            int code = (report.summary.result == BuildResult.Succeeded) ? 0 : 1;
+            EditorApplication.Exit (code);    
+    }
+
+    static void iOSRelease () {
+            PlayerSettings.SetScriptingBackend (BuildTargetGroup.iOS, ScriptingImplementation.IL2CPP);
+            PlayerSettings.applicationIdentifier = "com.pap.unityci";
+            PlayerSettings.SetScriptingDefineSymbolsForGroup (BuildTargetGroup.iOS, null);
+            EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTargetGroup.iOS, BuildTarget.iOS);
+            BuildReport report = BuildPipeline.BuildPlayer (GetScenes(), IOS_FOLDER, BuildTarget.iOS, BuildOptions.None);
+            int code = (report.summary.result == BuildResult.Succeeded) ? 0 : 1;
+            EditorApplication.Exit (code);    
+    }
+}
---
+++ SupportFiles/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+PROJECT_PATH="$(dirname "$(PWD)")"
+
+UNITY='/Applications/Unity/Hub/Editor/2020.1.0b12/Unity.app/Contents/MacOS/Unity'
+
+LOGS_PATH=$PROJECT_PATH'/Logs'
+ANDROID_PATH=$PROJECT_PATH'/Builds/Android'
+BUILDS_PATH=$PROJECT_PATH'/Builds'
+IOS_PATH=$PROJECT_PATH'/Builds/iOS'
+IOS_BUILD_PATH=$PROJECT_PATH'/Builds/iOS/build'
+IOS_DEVELOPMENT=$PROJECT_PATH'/Builds/iOS/build/development'
+IOS_RELEASE=$PROJECT_PATH'/Builds/iOS/build/release'
+INFOPLIST_FILE='Info.plist'
+
+SIGNING_IDENTITY="Apple Distribution: Chittapon Thongchim"
+PROVISIONING_PROFILE="4233980a-acb6-41b4-bf69-f724f5c90f2d"
+BUILD_NUMBER="1"
+
+if [ -d "$IOS_PATH" ]; then
+echo ''
+echo 'clean ios directory...' 
+echo ''
+BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$IOS_PATH/$INFOPLIST_FILE")
+rm -r $IOS_PATH
+fi
+echo "$IOS_PATH"
+echo ''
+echo 'build unity...' 
+echo '' 
+$UNITY -batchmode -quit -executeMethod BuildScript.iOSRelease -buildTarget ios -logFile "$LOGS_PATH/ios_release.log"
+if [ $? -ne 0 ]; then
+echo ''
+echo 'Operation failed!'
+echo '' 
+exit 1
+fi
+echo ''
+echo 'archive xcode...' 
+echo '' 
+BUILD_NUMBER=$(($BUILD_NUMBER + 1))
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "$IOS_PATH/$INFOPLIST_FILE"
+xcodebuild -project "$IOS_PATH/Unity-iPhone.xcodeproj" -scheme "Unity-iPhone" archive -archivePath "$IOS_RELEASE/Unity-iPhone.xcarchive" PROVISIONING_PROFILE_SPECIFIER="$PROVISIONING_PROFILE" CODE_SIGN_IDENTITY="$SIGNING_IDENTITY" -quiet > "$LOGS_PATH/ios_archive_release.log" 2>&1
+echo ''
+echo 'export ipa...' 
+echo '' 
+xcodebuild -exportArchive -archivePath "$IOS_RELEASE/Unity-iPhone.xcarchive" -exportOptionsPlist "release/options.plist" -exportPath $IOS_RELEASE -quiet > "$LOGS_PATH/ios_export_release.log" 2>&1
+echo ''
+echo 'validating...' 
+echo ''
+xcrun altool --validate-app -f "$IOS_RELEASE/Unity-iPhone.ipa" --apiKey Z23NQ47D86 --apiIssuer 47c78144-ee93-4eec-8f89-278d395255a5 —verbose
+echo ''
+echo 'uploading...' 
+echo ''
+xcrun altool --upload-app --file "$IOS_RELEASE/Unity-iPhone.ipa" --apiKey Z23NQ47D86 --apiIssuer 47c78144-ee93-4eec-8f89-278d395255a5 —verbose
